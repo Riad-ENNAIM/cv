cb724b1c999fcc7dd92669221fdbbc4c507dda74
Riad-ENNAIM <riad.ennaim.info@gmail.com>
2020-04-20 19:39:39 +0000

    Reviews Avatar: Romove Image & Get Color By Username

--- src/components/reviews/Review.js
+++ src/components/reviews/Review.js
@@ -1,13 +1,54 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import Rating from '../utils/Rating';
-import avatar from '../../images/avatar.png';
 
 const Review = ({ username, date, comment, rating }) => {
+  useEffect(() => {
+    const letterColors = {
+      a: 'F23',
+      b: 'E34',
+      c: 'F45',
+      d: 'AF6',
+      e: '5AB',
+      f: '678',
+      g: '789',
+      h: '890',
+      i: '90F',
+      j: 'FF2',
+      k: 'F23',
+      l: 'FE4',
+      m: 'DE5',
+      n: 'F96',
+      o: '567',
+      p: '678',
+      q: '789',
+      r: '890',
+      s: '90F',
+      t: 'FF2',
+      u: 'F23',
+      v: 'AF4',
+      w: 'CD5',
+      x: 'AB6',
+      y: 'ABC',
+      z: '678',
+    }
+    
+    const name = username.split(' ');
+    const firstLetter = name[0].slice(0,1);
+    const lastLetter = name[1].slice(0,1);
+    const colorNumber = '#'+ letterColors[firstLetter.toLowerCase()] + letterColors[lastLetter.toLowerCase()];
+    
+    setAvatar(firstLetter + lastLetter);
+    setAvatarColor(colorNumber)
+  }, [username]);
+
+  const [avatar, setAvatar] = useState(null);
+  const [avatarColor, setAvatarColor] = useState(null);
+
   return (
     <div className="review">
       <div className="review-header">
-        <img src={avatar} alt={username} />
+        <span className="review-avatar" style={{background: avatarColor}}>{avatar}</span>
         <div>
           <span className="review-date">{date}</span>
           <span className="review-username">{username}</span>
@@ -15,7 +56,7 @@ const Review = ({ username, date, comment, rating }) => {
       </div>
 
       <div className="review-content">
-        <Rating />
+        <Rating rating={rating}/>
         <p>
           {comment}
         </p>
--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -4,11 +4,11 @@ import Review from './Review';
 const Reviews = () => {
   return (
     <>
-      <Review />
+      <Review username="Adil Jalal" />
       <Review comment="Le lorem ipsum est, en imprimerie, une suite de mots sans signification utilisée à titre provisoire pour calibrer une mise en page,
         le texte définitif venant remplacer le faux-texte dès qu'il est prêt ou que la mise en page est achevée."
       />
-      <Review />
+      <Review username="Mohamed Ennaim" />
     </>
   );
 }
