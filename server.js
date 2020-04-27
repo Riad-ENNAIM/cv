8c6c90e33ccfd0135c73eb25dda664798f26ad42
Riad-ENNAIM <riad.ennaim.info@gmail.com>
2020-04-27 15:18:55 +0000

    Prepare For Deploy

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -11,6 +12,16 @@ app.use(express.json({ extended: false }));
 
 // Define Routes
 app.use('/api/reviews', require('./routes/reviews'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
