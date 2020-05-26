4ad18d4b7a71dc9f999746d16407b7bb0b2aa318
Riad-ENNAIM <riad.ennaim.info@gmail.com>
2020-05-26 15:29:01 +0000

    Fix Dark Mode Style

--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -15,11 +15,16 @@ const Header = () => {
     getProfile();
     if(language) document.title = `Riad ENNAIM | ${language.toUpperCase()}`;
     if(isDarkMode) {
-      document.documentElement.style.setProperty('--background-color', '#222222');
-      document.documentElement.style.setProperty('--primary-color', '#5D6D7E');
+      document.documentElement.style.setProperty('--background-color', '#1B2631');
+      document.documentElement.style.setProperty('--primary-color', '#D6DBDF');
       document.documentElement.style.setProperty('--dark-color', '#FFFFFF');
-      document.documentElement.style.setProperty('--light-color-degree1', '#555555');
-      document.documentElement.style.setProperty('--light-color-degree2', '#333333');
+      document.documentElement.style.setProperty('--light-color-degree1', '#17202A');
+      document.documentElement.style.setProperty('--light-color-degree2', '#5D6D7E');
+      document.documentElement.style.setProperty('--light-color-degree3', '#777777');
+      document.documentElement.style.setProperty('--btn-primary-background', '#34495E');
+      document.documentElement.style.setProperty('--btn-primary-color', '#D6DBDF');
+      document.documentElement.style.setProperty('--btn-disabled-background', '#D6DBDF');
+      document.documentElement.style.setProperty('--btn-disabled-color', '#AEB6BF');
     } else {
       document.documentElement.removeAttribute('style');
     }
